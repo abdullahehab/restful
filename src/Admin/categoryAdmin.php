cc7b80443b64969e78a0b7f97a4450e2b4f45deb
abdullahehab <nrtroz.ae@gmail.com>
2018-11-17 16:17:04 +0200

    create admin category class

--- src/Admin/categoryAdmin.php
+++ src/Admin/categoryAdmin.php
@@ -7,9 +7,36 @@
  */
 
 namespace App\Admin;
+use App\Entity\Category;
+use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Admin\AbstractAdmin;
 
-
-class categoryAdmin
+class categoryAdmin extends AbstractAdmin
 {
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper->add('name', TextareaType::class);
+    }
+
+  protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('name');
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('name', TextareaType::class);
+
+    }
+
+    public function toString($object)
+    {
+        return $object instanceof Category
+            ? $object->getName()
+            : 'Product'; // shown in the breadcrumb on the create view
+    }
 
 }
