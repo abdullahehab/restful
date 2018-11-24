d511607458e723fb99daf58f6c311d8713706860
abdullahehab <nrtroz.ae@gmail.com>
2018-11-24 13:29:29 +0200

    create TaskType class

--- src/Form/Type/TaskType.php
+++ src/Form/Type/TaskType.php
@@ -9,7 +9,30 @@
 namespace App\Form\Type;
 
 
-class TaskType
+use App\Entity\Task;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskType extends AbstractType
 {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('description');
+
+        $builder->add('tags', CollectionType::class, array(
+            'entry_type' => TagType::class,
+            'entry_options' => array('label' => false),
+        ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Task::class,
+        ));
+    }
+
 
 }
